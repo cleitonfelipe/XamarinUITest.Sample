94a8f8585c0cd2ea196b588e827fc9f69fe6e9b6
jamfenrol <jamfenrol@Mac-mini-de-jamfenrol.local>
2019-07-16 09:14:49 -0300

    First Commit

---
+++ Forms/UITest.Forms.Sample/UITest.Forms.Sample/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UITest.Forms.Sample.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Forms/UITest.Forms.Sample/UITest.Forms.Sample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UITest.Forms.Sample.Models;
+
+namespace UITest.Forms.Sample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
